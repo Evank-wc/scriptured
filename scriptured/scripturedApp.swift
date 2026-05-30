99a5096a46a656873cc0e4b621df7cf12c707693
whuo0004 <whuo0004@student.monash.edu>
2026-05-31 01:11:58 +1000

    Initial Commit

---
+++ scriptured/scripturedApp.swift
@@ -0,0 +1,17 @@
+//
+//  scripturedApp.swift
+//  scriptured
+//
+//  Created by Wen Cheng on 31/5/2026.
+//
+
+import SwiftUI
+
+@main
+struct scripturedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
